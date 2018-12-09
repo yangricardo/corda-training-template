0206a35463fcdb6318ab89232a603c578872a5b5
Yang Ricardo Barcellos Miranda <yang_ricardo@hotmail.com>
2018-12-09 21:32:44 -0200

    mustHandleMultipleCommandValues

--- kotlin-source/src/main/kotlin/net/corda/training/contract/IOUContract.kt
+++ kotlin-source/src/main/kotlin/net/corda/training/contract/IOUContract.kt
@@ -24,7 +24,9 @@ class IOUContract : Contract {
         // E.g
         // class DoSomething : TypeOnlyCommandData(), Commands
 
-        class Issue : TypeOnlyCommandData(), CommandData
+        class Issue : TypeOnlyCommandData(), Commands
+        class Transfer : TypeOnlyCommandData(), Commands
+        class Settle : TypeOnlyCommandData(), Commands
     }
 
     /**
@@ -32,19 +34,24 @@ class IOUContract : Contract {
      * The constraints are self documenting so don't require any additional explanation.
      */
     override fun verify(tx: LedgerTransaction) {
-        val command = tx.commands.requireSingleCommand<Commands.Issue>()
+        val command = tx.commands.requireSingleCommand<IOUContract.Commands>()
         // Add contract code here.
         // requireThat {
         //     ...
         // }
-        requireThat {
-            "No inputs should be consumed when issuing an IOU." using (tx.inputs.isEmpty())
-            "Only one output state should be created when issuing an IOU." using (tx.outputs.size == 1)
-            val iou = tx.outputStates.single() as IOUState
-            "A newly issued IOU must have a positive amount." using (iou.amount.quantity > 0)
-            "The lender and borrower cannot have the same identity." using (iou.lender != iou.borrower)
-            "Both lender and borrower together only may sign IOU issue transaction." using (command.signers.toSet() == iou.participants.map { it.owningKey }.toSet())
+        when (command.value) {
+            is Commands.Issue -> requireThat {
+                "No inputs should be consumed when issuing an IOU." using (tx.inputs.isEmpty())
+                "Only one output state should be created when issuing an IOU." using (tx.outputs.size == 1)
+                val iou = tx.outputStates.single() as IOUState
+                "A newly issued IOU must have a positive amount." using (iou.amount.quantity > 0)
+                "The lender and borrower cannot have the same identity." using (iou.lender != iou.borrower)
+                "Both lender and borrower together only may sign IOU issue transaction." using (command.signers.toSet() == iou.participants.map { it.owningKey }.toSet())
+            }
+            is Commands.Transfer -> requireThat {  }
+            is Commands.Settle -> requireThat {  }
         }
+
 
     }
 }
